ed9b069a8ef6cc25d4f17af56f1da105417d1230
Andika Erwansyah <andika.erwansyah@gmail.com>
2020-06-12 00:50:15 +0700

    Review Traits On PHP

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -7,10 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 trait ApiResponser {
 
+    /**
+     * if response is successfully established
+     *
+     * @param array $data
+     * @param number $code
+     * @return void
+     */
     private function successResponse($data, $code){
         return response()->json($data, $code);
     }
 
+    /**
+     * handle error response
+     *
+     * @param string $message
+     * @param number $code
+     * @return void
+     */
     protected function errorResponse($message, $code){
         return response()->json([
             'error' => $message,
@@ -18,12 +32,26 @@ trait ApiResponser {
         ], $code);
     }
 
+    /**
+     * Return multiple data from the request
+     *
+     * @param Collection $collection
+     * @param integer $code
+     * @return void
+     */
     protected function showAll(Collection $collection, $code = 200){
         return $this->successResponse([
             'data' => $collection,
         ], $code);
     }
 
+    /**
+     * Return single value of a request
+     *
+     * @param Model $model
+     * @param integer $code
+     * @return void
+     */
     protected function showOne(Model $model, $code = 200){
         return $this->successResponse([
             'data' => $model

d05918145873f2a7d3a7952c131265577cceadce
Andika Erwansyah <andika.erwansyah@gmail.com>
2020-06-12 13:43:12 +0700

    New routes
    new routes for
    which buyer buy product from which seller
    which buyer buy product category

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,8 @@ Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]
 Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
 Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
 Route::resource('buyers.products', 'Buyer\BuyerProductController', ['only' => ['index']]);
+Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => ['index']]);
+Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' => ['index']]);
 
 /**
  * Sellers
